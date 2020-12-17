a5ebc7e7354ee5aee1e7481e506e3525de9d6cb6
Nour <nouralchawa@example.com>
2020-12-18 00:36:44 +0100

    Refactorización de consulta api ombd

--- main.py
+++ main.py
@@ -1,55 +1,35 @@
 import requests
 
 API_KEY="f36cb021"
-url_busqueda = "http://www.omdbapi.com/?apikey={}&s={}"
-url_identificador = "http://www.omdbapi.com/?apikey={}&i={}"
+url_template = "http://www.omdbapi.com/?apikey={}&{}={}"
+
 
 def peticion(url):
-    respuesta = requests.get(direccion)
+    respuesta = requests.get(url)
     if respuesta.status_code == 200:
         datos = respuesta.json()
         if datos['Response'] == "False":
-            print(datos["Error"])
+            return datos["Error"]
         else:
             return datos
     else:
-        print("Error en consulta:", nueva_respuesta.status_code)
+        return "Error en consulta por id: ", respuesta.status_code
 
 
 pregunta= input("Titulo de la película: ")
 
-respuesta = peticion(url_busqueda.format(APY_KEY, pregunta))
-primera_peli = respuesta['Search'][0]
-clave = primera_peli['imdbID']
-
-respuesta = peticion(url_identificador.format(APY_KEY, clave))
-titulo = respuesta['Title']
-agno = respuesta['Year']
-director = respuesta['Director']
-print("La película {}, estrenadada en el año {}, fue dirigida por {}".format(titulo, agno, director))
-
-
-
-if respuesta.status_code == 200:
-    datos = respuesta.json()
-    if datos['Response'] == "False":
-        print(datos["Error"])
-    else:
-        primera_peli = datos['Search'][0]
-        clave = primera_peli['imdbID']
-
-        otra_direccion = f"http://www.omdbapi.com/?apikey={API_KEY}&i={clave}"
-        nueva_respuesta = requests.get(otra_direccion)
-        if nueva_respuesta.status_code == 200:
-            datos = nueva_respuesta.json()
-            if datos['Response'] == "False":
-                print(datos["Error"])
-            else:
-                titulo = datos['Title']
-                agno = datos['Year']
-                director = datos['Director']
-                print("La película {}, estrenadada en el año {}, fue dirigida por {}".format(titulo, agno, director))
-        else:
-            print("Error en consulta:", nueva_respuesta.status_code)
+respuesta = peticion(url_template.format(API_KEY, 's', pregunta))
+if isinstance(respuesta, str):
+    print(respuesta)
 else:
-    print("Error en busqueda:", respuesta.status_code)
+    primera_peli = respuesta['Search'][0]
+    clave = primera_peli['imdbID']
+
+    respuesta = peticion(url_template.format(API_KEY, 'i', clave))
+    if isinstance(respuesta, str):
+        print(respuesta)
+    else:
+        titulo = respuesta['Title']
+        agno = respuesta['Year']
+        director = respuesta['Director']
+        print("La película {}, estrenadada en el año {}, fue dirigida por {}".format(titulo, agno, director))
